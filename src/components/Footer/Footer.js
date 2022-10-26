b600851c895c9cdd2985b5753a4fce4621cf589d
HamimHossain <hamim.mgp@gmail.com>
2022-10-26 18:01:09 +0600

    footer done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            <h3>This is footer section</h3>
+        <div className='bg-dark mt-5 d-flex justify-content-center align-items-center' style={{height:'200px'}}>
+            <p className='text-white'><small>Copyright&copy; E-programming University 2022. </small></p>
         </div>
     );
 };
